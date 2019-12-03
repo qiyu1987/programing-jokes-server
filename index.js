2ddaf9cab2bbfed8e9c6a14196357e0cf914abbb
qiyu1987 <qiyu1987@gmail.com>
2019-12-03 23:39:53 +0100

    insert 10 jokes in db

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require("express")
 const app = express()
 const port = process.env.PORT || 4000
+const request = require("superagent")
 const User = require("./user/model")
 const Joke = require("./joke/model")
-const jokes = app.listen(port, () => {
+// request
+// 	.get("https://official-joke-api.appspot.com/jokes/programming/ten")
+// 	.then(res => {
+// 		const jokePromise = Joke.bulkCreate(res.body)
+// 	})
+app.listen(port, () => {
 	console.log(`listening on port ${port}`)
 })
