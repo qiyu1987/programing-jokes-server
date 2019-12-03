33fa14ede7dd4ef417e04c2352cfef7dbdf36df9
qiyu1987 <qiyu1987@gmail.com>
2019-12-04 00:01:57 +0100

    post new joke

--- index.js
+++ index.js
@@ -5,11 +5,15 @@ const port = process.env.PORT || 4000
 const User = require("./user/model")
 const Joke = require("./joke/model")
 const jokeRouter = require("./joke/router")
+
+const bodyPaser = require("body-parser")
+const jsonParser = bodyPaser.json()
 // request
 // 	.get("https://official-joke-api.appspot.com/jokes/programming/ten")
 // 	.then(res => {
 // 		const jokePromise = Joke.bulkCreate(res.body)
 // 	})
+app.use(jsonParser)
 app.use(jokeRouter)
 app.listen(port, () => {
 	console.log(`listening on port ${port}`)
--- joke/router.js
+++ joke/router.js
@@ -14,4 +14,16 @@ router.get("/jokes/random", async (req, res, next) => {
 		next(err)
 	}
 })
+router.post("/jokes", async (req, res, next) => {
+	try {
+		const joke = await Joke.create(req.body)
+		if (joke) {
+			res.json(joke).status(201)
+		} else {
+			res.status(404).end()
+		}
+	} catch (err) {
+		next(err)
+	}
+})
 module.exports = router
