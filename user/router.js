626dfbeb1a32db6244663f7186bf9e957fd716dc
qiyu1987 <qiyu1987@gmail.com>
2019-12-12 14:38:11 +0100

    not send back password

--- user/router.js
+++ user/router.js
@@ -1,13 +1,18 @@
 const { Router } = require("express")
 const Sequelize = require("sequelize")
+const bcrypt = require("bcrypt")
 const User = require("./model")
 const router = new Router()
 
 router.post("/users", async (req, res, next) => {
 	try {
 		console.log("post /users request")
-		const user = await User.create(req.body)
-		res.json(user)
+		const { username, password } = req.body
+		const user = await User.create({
+			username,
+			password: bcrypt.hashSync(password, 10)
+		})
+		res.status(201).send({ message: "user created successfully!" })
 	} catch {
 		next
 	}
