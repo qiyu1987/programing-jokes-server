d4884f6936f628557aa9b38c06ff7b2ce26f83da
qiyu1987 <qiyu1987@gmail.com>
2019-12-12 14:08:31 +0100

    modify user model

--- user/model.js
+++ user/model.js
@@ -1,7 +1,17 @@
 const Sequelize = require("sequelize")
 const db = require("../db")
-const User = db.define("user", {
-	username: Sequelize.STRING,
-	password: Sequelize.STRING
-})
+const User = db.define(
+	"user",
+	{
+		username: {
+			type: Sequelize.STRING,
+			allowNull: false
+		},
+		password: {
+			type: Sequelize.STRING,
+			allowNull: false
+		}
+	},
+	{ timestamps: false }
+)
 module.exports = User
